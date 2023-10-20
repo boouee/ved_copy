0f2168c8f6cf4deeaef7ff49549d58f6b75a5afc
boouee <104202645+boouee@users.noreply.github.com>
2023-10-20 13:51:16 +0300

    Add files via upload

---
+++ .eslintrc.cjs
@@ -0,0 +1,45 @@
+/** @type {import("eslint").Linter.Config} */
+const config = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: true,
+  },
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "next/core-web-vitals",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked",
+    "prettier",
+  ],
+  rules: {
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "warn", // or "error"
+      {
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+        caughtErrorsIgnorePattern: "^_",
+      },
+    ],
+    "new-cap": 0,
+    eqeqeq: ["error", "smart"],
+    "max-len": [
+      "error",
+      {
+        code: 140,
+        tabWidth: 2,
+        ignoreComments: true, //"comments": 80
+        ignoreUrls: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+        ignoreRegExpLiterals: true,
+      },
+    ],
+    "no-var": "warn",
+    "no-nested-ternary": "warn",
+    "no-template-curly-in-string": "warn",
+    "react-hooks/exhaustive-deps": "off",
+  },
+};
+
+module.exports = config;
