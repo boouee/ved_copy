24dde4574831f0de60dee45f17d20949354edff5
boouee <104202645+boouee@users.noreply.github.com>
2023-10-21 15:18:48 +0300

    Update CutPlane.tsx

--- src/components/three/objects/CutPlane.tsx
+++ src/components/three/objects/CutPlane.tsx
@@ -102,7 +102,7 @@ export default function CutPlane({ ...props }: CutPlaneProps) {
         {...props}
       >
         <planeGeometry args={[10, 10, 10, 10]} />
-        <meshStandardMaterial color={"green"} wireframe />
+        <meshStandardMaterial color={"white"} wireframe />
       </mesh>
       <CutPlaneRotation />
     </>
