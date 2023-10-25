760ad20476b8d2df8c12d21f8709e2a633eef8aa
boouee <104202645+boouee@users.noreply.github.com>
2023-10-25 18:21:26 +0300

    Update ObjectMesh.tsx

--- src/components/three/objects/ObjectMesh.tsx
+++ src/components/three/objects/ObjectMesh.tsx
@@ -47,7 +47,7 @@ export function ObjectMesh({ mesh, json }: ObjectInfo) {
 
     setIsMeshHovered(true);
   };
-  const texture = new three.TextureLoader().load( 'sprite.png' );
+  const texture = new TextureLoader().load( 'sprite.png' );
   const onPointerOut = () => setIsMeshHovered(false);
 
   const meshRef = useRef<Mesh>(null!);
