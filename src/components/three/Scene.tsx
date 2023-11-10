45ae27a0953ab8ef6c29b9abb003d97a785b9581
boouee <104202645+boouee@users.noreply.github.com>
2023-11-10 15:30:29 +0300

    Update Scene.tsx

--- src/components/three/Scene.tsx
+++ src/components/three/Scene.tsx
@@ -9,7 +9,7 @@ import { TextureLoader } from 'three/src/loaders/TextureLoader';
 import SceneObjects from "~/components/three/SceneObjects";
 
 export default function Scene() {
-  const colorMap = useLoader(TextureLoader, 'textures/albedo.jpg')
+  const colorMap =  await textureLoader.loadAsync('textures/albedo.jpg')
   return (
     <Canvas camera={{ fov: 75, near: 0.1, far: 3000, position: [15, 15, 15] }}>
       <InitialProps />
