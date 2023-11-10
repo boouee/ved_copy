c68667fd7c268840ecd8590998d6fe8890d04a93
boouee <104202645+boouee@users.noreply.github.com>
2023-11-10 15:10:36 +0300

    Update Scene.tsx

--- src/components/three/Scene.tsx
+++ src/components/three/Scene.tsx
@@ -1,35 +1,37 @@
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useLoader } from "@react-three/fiber";
 import InitialProps from "~/components/three/environment/InitialProps";
 import Lighting from "~/components/three/environment/Lighting";
 import ObjectSelector from "~/components/three/gui/ObjectSelector";
 import Utils from "~/components/three/gui/Utils";
 import { DoubleSide } from "three";
+import { TextureLoader } from 'three/src/loaders/TextureLoader';
 
 import SceneObjects from "~/components/three/SceneObjects";
 
 export default function Scene() {
+  const colorMap = useLoader(TextureLoader, './worn-modern-panels2_albedo.png')
   return (
     <Canvas camera={{ fov: 75, near: 0.1, far: 3000, position: [15, 15, 15] }}>
       <InitialProps />
       <ambientLight intensity={1} />
       <mesh position ={[0,0,0]} rotation = {[0, Math.PI/2,0]}>
-          <planeGeometry attach="geometry" args={[25, 15]} />
+          <planeGeometry attach="geometry" args={[25, 7]} />
+          <meshBasicMaterial attach="material"  map={colorMap} color="green" side={DoubleSide}/>
+      </mesh>
+      <mesh position ={[0,0,0]}>
+          <planeGeometry attach="geometry" args={[25, 7]} />
           <meshBasicMaterial attach="material" color="green" side={DoubleSide}/>
       </mesh>
       <mesh position ={[0,0,0]}>
-          <planeGeometry attach="geometry" args={[25, 15]} />
+          <planeGeometry attach="geometry" args={[25, 7]} />
           <meshBasicMaterial attach="material" color="green" side={DoubleSide}/>
       </mesh>
       <mesh position ={[0,0,0]}>
-          <planeGeometry attach="geometry" args={[25, 15]} />
+          <planeGeometry attach="geometry" args={[25, 7]} />
           <meshBasicMaterial attach="material" color="green" side={DoubleSide}/>
       </mesh>
       <mesh position ={[0,0,0]}>
-          <planeGeometry attach="geometry" args={[25, 15]} />
-          <meshBasicMaterial attach="material" color="green" side={DoubleSide}/>
-      </mesh>
-      <mesh position ={[0,0,0]}>
-          <planeGeometry attach="geometry" args={[25, 15]} />
+          <planeGeometry attach="geometry" args={[25, 7]} />
           <meshBasicMaterial attach="material" color="green" side={DoubleSide}/>
       </mesh>
     </Canvas>
