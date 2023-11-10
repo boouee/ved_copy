5dfd2de858540d0e75f9bb97e9bb5674935c765e
boouee <104202645+boouee@users.noreply.github.com>
2023-11-10 14:43:13 +0300

    Update Scene.tsx

--- src/components/three/Scene.tsx
+++ src/components/three/Scene.tsx
@@ -3,6 +3,7 @@ import InitialProps from "~/components/three/environment/InitialProps";
 import Lighting from "~/components/three/environment/Lighting";
 import ObjectSelector from "~/components/three/gui/ObjectSelector";
 import Utils from "~/components/three/gui/Utils";
+import { THREE } from "three";
 
 import SceneObjects from "~/components/three/SceneObjects";
 
@@ -14,7 +15,7 @@ export default function Scene() {
       <directionalLight color="red" position={[0, 0, 5]} />
       <mesh position ={[0,0,0]}>
           <planeGeometry attach="geometry" args={[25, 15]} />
-          <meshPhongMaterial attach="material" color="green" />
+          <meshPhongMaterial attach="material" color="green" side={THREE.BackSide}/>
       </mesh>
     </Canvas>
   );
