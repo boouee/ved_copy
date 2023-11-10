31e25c3735d5ab9bbf4c4d0418d712c5f61512b2
boouee <104202645+boouee@users.noreply.github.com>
2023-11-10 14:09:45 +0300

    Update Scene.tsx

--- src/components/three/Scene.tsx
+++ src/components/three/Scene.tsx
@@ -8,12 +8,13 @@ import SceneObjects from "~/components/three/SceneObjects";
 
 export default function Scene() {
   return (
-    <Canvas camera={{ fov: 75, near: 0.1, far: 3000, position: [15, 15, 15] }}>
-      <InitialProps />
-      <Lighting />
-      <Utils />
-      <SceneObjects />
-      <ObjectSelector />
+    <Canvas>
+      <ambientLight intensity={0.1} />
+      <directionalLight color="red" position={[0, 0, 5]} />
+      <mesh>
+        <boxGeometry />
+        <meshStandardMaterial />
+      </mesh>
     </Canvas>
   );
 }
