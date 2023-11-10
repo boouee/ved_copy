edd719223ce63ecf59b8b13e6db08797ceb9d996
boouee <104202645+boouee@users.noreply.github.com>
2023-11-10 15:51:09 +0300

    Update Scene.tsx

--- src/components/three/Scene.tsx
+++ src/components/three/Scene.tsx
@@ -1,4 +1,5 @@
 import { Canvas, useLoader } from "@react-three/fiber";
+import { Suspense } from 'react'
 import InitialProps from "~/components/three/environment/InitialProps";
 import Lighting from "~/components/three/environment/Lighting";
 import ObjectSelector from "~/components/three/gui/ObjectSelector";
@@ -8,38 +9,34 @@ import { TextureLoader } from 'three/src/loaders/TextureLoader';
 import useTextureSetup from "~/hooks/useTextureSetup";
 import SceneObjects from "~/components/three/SceneObjects";
 const textureLoader = new TextureLoader();
-const colorMap =  await textureLoader.loadAsync('~/textures/albedo.jpg');
+const colorMap =  TextureLoader('~/textures/albedo.jpg');
 export default function Scene() {
-  const { setupTexture } = useTextureSetup({ mesh, json });
-
-
-  useEffect(() => {
-    void setupTexture();
-  });  
   return (
     <Canvas camera={{ fov: 75, near: 0.1, far: 3000, position: [15, 15, 15] }}>
-      <InitialProps />
-      <ambientLight intensity={1} />
-      <mesh position ={[0,0,0]} rotation = {[0, Math.PI/2,0]}>
+      <Suspense fallback={null}>
+        <InitialProps />
+        <ambientLight intensity={1} />
+        <mesh position ={[0,0,0]} rotation = {[0, Math.PI/2,0]}>
           <planeGeometry attach="geometry" args={[25, 7]} />
           <meshBasicMaterial attach="material" color="green" side={DoubleSide}/>
-      </mesh>
-      <mesh position ={[0,0,0]}>
+        </mesh>
+        <mesh position ={[0,0,0]}>
           <planeGeometry attach="geometry" args={[25, 7]} />
           <meshBasicMaterial attach="material" color="green" side={DoubleSide}/>
-      </mesh>
-      <mesh position ={[0,0,0]}>
+        </mesh>
+        <mesh position ={[0,0,0]}>
           <planeGeometry attach="geometry" args={[25, 7]} />
           <meshBasicMaterial attach="material" color="green" side={DoubleSide}/>
-      </mesh>
-      <mesh position ={[0,0,0]}>
+        </mesh>
+        <mesh position ={[0,0,0]}>
           <planeGeometry attach="geometry" args={[25, 7]} />
           <meshBasicMaterial attach="material" color="green" side={DoubleSide}/>
-      </mesh>
-      <mesh position ={[0,0,0]}>
+        </mesh>
+        <mesh position ={[0,0,0]}>
           <planeGeometry attach="geometry" args={[25, 7]} />
           <meshBasicMaterial attach="material" color="green" side={DoubleSide}/>
-      </mesh>
+        </mesh>
+      </Suspense>
     </Canvas>
   );
 }
