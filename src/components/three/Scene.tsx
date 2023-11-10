8945bae7927e823668aa13fa5f8255f352b2f5d0
boouee <104202645+boouee@users.noreply.github.com>
2023-11-10 15:22:41 +0300

    Update Scene.tsx

--- src/components/three/Scene.tsx
+++ src/components/three/Scene.tsx
@@ -9,14 +9,14 @@ import { TextureLoader } from 'three/src/loaders/TextureLoader';
 import SceneObjects from "~/components/three/SceneObjects";
 
 export default function Scene() {
-  const colorMap = useLoader(TextureLoader, 'albedo.jpg')
+  //const colorMap = useLoader(TextureLoader, 'albedo.jpg')
   return (
     <Canvas camera={{ fov: 75, near: 0.1, far: 3000, position: [15, 15, 15] }}>
       <InitialProps />
       <ambientLight intensity={1} />
       <mesh position ={[0,0,0]} rotation = {[0, Math.PI/2,0]}>
           <planeGeometry attach="geometry" args={[25, 7]} />
-          <meshBasicMaterial attach="material"  map={colorMap} color="green" side={DoubleSide}/>
+          <meshBasicMaterial attach="material"  map={map} color="green" side={DoubleSide}/>
       </mesh>
       <mesh position ={[0,0,0]}>
           <planeGeometry attach="geometry" args={[25, 7]} />
